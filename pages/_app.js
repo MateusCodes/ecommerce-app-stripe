329ac26998322cb9e49d332323ec34c0c18a2107
Mateus Sobreira <matheus.sobreira.27@gmail.com>
2022-08-01 03:55:01 -0300

    add nice loading effect

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,32 @@ import '../styles/global.css';
 import Layout from '../components/Layout';
 import { StateContext } from '../context/StateContext';
 import { Toaster } from 'react-hot-toast';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+    NProgress.configure({ showSpinner: false });
+
+    Router.events.on('routeChangeStart', () => {
+        NProgress.start();
+    });
+
+    Router.events.on('routeChangeComplete', () => {
+        NProgress.done();
+    });
+
     return (
         <StateContext>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+                    integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+                    crossOrigin="anonymous"
+                    referrerPolicy="no-referrer"
+                />
+            </Head>
             <Layout>
                 <Toaster />
                 <Component {...pageProps} />
